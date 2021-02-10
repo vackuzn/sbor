f50a4c5bd5fcf28bb2e53ea35ae258147ac916ba
afa-spb <skorobey.spb@mail.ru>
2021-02-10 15:18:03 +0400

    updated categories, added page category

--- internet_shop/main/views.py
+++ internet_shop/main/views.py
@@ -2,14 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from main.models import Category
 
+
 def main_page(request):
     return render(request, 'main/main_page.html')
+
 
 def categories(request):
     all_categories = Category.objects.all()
     context = {'all_categories': all_categories}
     return render(request, 'main/categories.html', context)
 
+
 def category(request, id):
     category = Category.objects.get(id=id)
-    return HttpResponse(category.title)
+    context = {'category': category}
+    return render(request, 'main/category.html', context)
