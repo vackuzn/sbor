450f5a6630e152601183a6d7df175b1171ff7623
afa-spb <skorobey.spb@mail.ru>
2021-02-27 21:53:30 +0400

    added one product

--- internet_shop/cart/forms.py
+++ internet_shop/cart/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class CartAddProductForm(forms.Form):
     id_product = forms.IntegerField(label='id продукта', widget=forms.HiddenInput)
-    quantity = forms.IntegerField(label='Количество', widget=forms.NumberInput(attrs={'class': 'cart_quantity'}))
+    quantity = forms.IntegerField(label='Количество', min_value=0, widget=forms.NumberInput(attrs={'class': 'cart_quantity'}))
