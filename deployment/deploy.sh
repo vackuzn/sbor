cfb05a649c5fb74cbacce1a2ba384357d7e41381
Vyacheslav Kuznetsov <o-seer@mail.ru>
2021-04-17 20:38:40 +0200

    Added deployment

---
+++ deployment/deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Settings
+BASE_FOLDER=~
+HOST_FOLDER="$BASE_FOLDER/public_html"
+VENV_FOLDER="$BASE_FOLDER/venvs/prod"
+TEMP_REPO_FOLDER="/tmp/deploy"
+PROJECT_FOLDER="internet_shop"
+
+# Functions
+print () {
+	echo
+	echo $1
+}
+
+# Main code
+print "Recreating virtual environment and activating it..."
+rm -rf $VENV_FOLDER
+python3 "$BASE_FOLDER/virtualenv/virtualenv.py" $VENV_FOLDER
+source $VENV_FOLDER/bin/activate
+
+print "Cloning repo"
+rm -rf $TEMP_REPO_FOLDER
+git clone https://github.com/o-seer/sbormarket.git $TEMP_REPO_FOLDER
+
+print "Installing dependencies"
+pip install -r $TEMP_REPO_FOLDER/requirements.txt
+
+print "Cleanup of existing installation"
+cd $HOST_FOLDER
+rm -rf $PROJECT_FOLDER
+
+print "Copying new files"
+mv $TEMP_REPO_FOLDER/$PROJECT_FOLDER .
+
+cd internet_shop
+print "Running collectstatic"
+python manage.py collectstatic --noinput
+print "Applying migrations"
+python manage.py migrate
+
+print "Cleanup"
+cd ~
+rm -rf $TEMP_REPO_FOLDER
+deactivate
