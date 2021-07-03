7a114cbdf3bcbb88c11fc7d348241ea5a38535fe
Vyacheslav Kuznetsov <o-seer@mail.ru>
2021-07-03 11:49:33 +0200

    Implemented settings init

--- internet_shop/main/context_processors/main_context_processor.py
+++ internet_shop/main/context_processors/main_context_processor.py
@@ -2,6 +2,8 @@ from main.models import SiteSettings
 
 
 def main(request):
+    init_settings()
+
     context = {
         'min_sum': int(SiteSettings.objects.get(id=1).value),
         'contacts_telephone': SiteSettings.objects.get(key_title='contacts_telephone').value,
@@ -10,3 +12,22 @@ def main(request):
         'contacts_for_order': SiteSettings.objects.get(key_title='contacts_for_order').value,
     }
     return context
+
+
+def init_settings():
+    if SiteSettings.objects.count() > 0:
+        return
+
+    default_settings = [
+        SiteSettings(key_title="min_sum", description="Минимальная сумма заказа", value="1200"),
+        SiteSettings(key_title="contacts_telephone", description="Телефон в шапке сайта", value="+79817363599"),
+        SiteSettings(key_title="contacts_time", description="Время работы в шапке сайта",
+                     value="Доставка ежедневно с 9:00 до 21:00"),
+        SiteSettings(key_title="contacts_email", description="Адрес электронной почты в шапке сайта",
+                     value="sbor-market@mail.ru"),
+        SiteSettings(key_title="contacts_for_order", description="Телефон в случае отсутствия товара на складе",
+                     value="+7 981 736 3599"),
+    ]
+
+    for setting in default_settings:
+        setting.save()
